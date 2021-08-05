bd5abefd0c9a5fbff8ac4270176b39fb17e54d8d
Eliel M. Gaspar <elielmichel@gmail.com>
2021-08-05 20:18:26 -0300

    chore: create static file serving for avatars

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import express from 'express';
 import routes from './routes';
 
 import './database';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.diretory));
 app.use(routes);
 
 app.listen(3333, () => {
