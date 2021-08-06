b5e1361c2b9a294b0a6b483a00ad840ce00a34d0
Eliel M. Gaspar <elielmichel@gmail.com>
2021-08-06 16:32:07 -0300

    chore: start SOLID implementation

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,12 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import Appointment from '../entities/Appointment';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
-  public async findByDate(date: Date): Promise<Appointment | null> {
+class AppointmentsRepository
+  extends Repository<Appointment>
+  implements IAppointmentsRepository
+{
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     const findAppointment = await this.findOne({ date });
 
-    return findAppointment ? findAppointment : null;
+    return findAppointment;
   }
 }
 
