ce7592014abd64ea5a206a554577c4927df26cee
Eliel M. Gaspar <elielmichel@gmail.com>
2021-08-17 17:54:53 -0300

    feat: create appointment module tests

--- src/modules/appointments/services/CreateAppointment.service.spec.ts
+++ src/modules/appointments/services/CreateAppointment.service.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentRepository';
 import CreateAppointmentsService from './CreateAppointment.service';
 
@@ -16,7 +17,24 @@ describe('CreateAppointments', () => {
     expect(appointment).toHaveProperty('id');
   });
 
-  // it('should not be able to create to a new appointments on the same date', () => {
-  //   expect();
-  // });
+  it('should not be able to create to a new appointments on the same date', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentsService(
+      fakeAppointmentRepository
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123123123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123123123',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
