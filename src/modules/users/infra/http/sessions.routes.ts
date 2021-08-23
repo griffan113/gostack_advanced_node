d5e58b6aff14e02cc5b7b6acd82b9d668db0d61c
Eliel <70238970+griffan113@users.noreply.github.com>
2021-08-23 09:15:48 -0300

    fix(tsyringe): fix internal server errors bugs

--- src/modules/users/infra/http/sessions.routes.ts
+++ src/modules/users/infra/http/sessions.routes.ts
@@ -1,7 +1,5 @@
 import { Router } from 'express';
 
-import AuthenticateUserService from '@modules/users/services/AuthenticateUser.service';
-import { container } from 'tsyringe';
 import SessionsController from './controllers/Sessions.controller';
 
 const sessionsRouter = Router();
