7d692bac6a795258286e9539681f2414f6430a67
Eliel <70238970+griffan113@users.noreply.github.com>
2021-08-26 10:43:47 -0300

    chore: finish ListProviderMonthAvailability tests

--- src/modules/appointments/services/ListProviderMonthAvailability.service.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvailability.service.spec.ts
@@ -13,15 +13,12 @@ describe('ListProviderMonthAvailability', () => {
   });
 
   it('should be able to return the available providers monthly', async () => {
-    await fakeAppointmentRepository.create({
-      provider_id: 'provider_id',
-      date: new Date(2020, 4, 20, 8, 0, 0),
-    });
-
-    await fakeAppointmentRepository.create({
-      provider_id: 'provider_id',
-      date: new Date(2020, 4, 20, 10, 0, 0),
-    });
+    for (let hour = 8; hour <= 17; hour++) {
+      await fakeAppointmentRepository.create({
+        provider_id: 'provider_id',
+        date: new Date(2020, 4, 20, hour, 0, 0),
+      });
+    }
 
     await fakeAppointmentRepository.create({
       provider_id: 'provider_id',
@@ -36,9 +33,9 @@ describe('ListProviderMonthAvailability', () => {
 
     expect(avaliability).toEqual(
       expect.arrayContaining([
-        { day: 19, avaliable: false },
+        { day: 19, avaliable: true },
         { day: 20, avaliable: false },
-        { day: 21, avaliable: false },
+        { day: 21, avaliable: true },
         { day: 22, avaliable: true },
       ])
     );
--- src/modules/appointments/services/ListProviderMonthAvailability.service.ts
+++ src/modules/appointments/services/ListProviderMonthAvailability.service.ts
@@ -1,3 +1,4 @@
+import { getDate, getDaysInMonth } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -32,9 +33,25 @@ class ListProviderMonthAvailabilityService {
         year,
       });
 
-    console.log(appointmentsInMonth);
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month, -1));
 
-    return [{ avaliable: false, day: 10 }];
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1
+    );
+
+    const avaliability = eachDayArray.map((day) => {
+      const appointmentsInDay = appointmentsInMonth.filter((appointment) => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        avaliable: appointmentsInDay.length < 10,
+      };
+    });
+
+    return avaliability;
   }
 }
 
